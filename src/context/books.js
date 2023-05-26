7923fc2b6ab587fe2d9e0bebc069a7e9042baf66
Helen <lena.tsigipalo@gmail.com>
2023-05-26 19:41:17 +0200

    first commit

---
+++ src/context/books.js
@@ -0,0 +1,65 @@
+import {createContext, useCallback, useState} from "react";
+import axios from "axios";
+
+const BooksContext = createContext();
+
+const Provider = ({ children }) => {
+    const [ books, setBooks ] = useState([]);
+
+    const fetchBooks = useCallback(async () => {
+        const response = await axios.get('http://localhost:3001/books');
+
+        setBooks(response.data);
+    }, []);
+
+    const deleteBookById = async (id) => {
+        await axios.delete(`http://localhost:3001/books/${id}`);
+
+        const updatedBooks = books.filter(book => {
+            return book.id !== id;
+        });
+
+        setBooks(updatedBooks);
+    };
+
+    const editBookById = async (id, title) => {
+        const response = await axios.put(`http://localhost:3001/books/${id}`, { title });
+
+        const updatedBooks = books.map(book => {
+            if (book.id === id) {
+                return { ...book, ...response.data }
+            };
+
+            return book;
+        });
+
+        setBooks(updatedBooks);
+    };
+
+    const createBook = async (title) => {
+        const response = await axios.post('http://localhost:3001/books', { title });
+
+        console.log(response);
+        const updatedBooks = [
+            ...books,
+            response.data
+        ];
+
+        setBooks(updatedBooks);
+    };
+
+    const  valueToShare = {
+        books,
+        fetchBooks,
+        deleteBookById,
+        editBookById,
+        createBook
+    };
+
+    return <BooksContext.Provider value={valueToShare}>
+            {children}
+        </BooksContext.Provider>
+}
+
+export {Provider};
+export default BooksContext;
